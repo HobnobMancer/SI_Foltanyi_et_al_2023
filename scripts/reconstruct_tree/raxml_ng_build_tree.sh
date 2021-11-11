1c8c06d4916b85c280d4f91b18f259991e465595
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-11-11 14:59:12 +0000

    update script file permissions

---
+++ scripts/reconstruct_tree/raxml_ng_build_tree.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# raxml_ng_build_tree.sh
+#
+# Build maximum parsimony tree using raxml-ng, with
+# bootstrap support
+
+raxml-ng --check \
+  --msa concatenated_cds/concatenated.fasta \
+  --model concatenated_cds/concatenated.part \
+  --prefix tree/01_check
+
+raxml-ng --parse \
+  --msa concatenated_cds/concatenated.fasta \
+  --model concatenated_cds/concatenated.part \
+  --prefix tree/02_parse
+
+raxml-ng \
+  --msa concatenated_cds/concatenated.fasta \
+  --model concatenated_cds/concatenated.part \
+  --threads 8 \
+  --seed 38745 \
+  --prefix tree/03_infer
+
+raxml-ng --bootstrap \
+  --msa concatenated_cds/concatenated.fasta \
+  --model concatenated_cds/concatenated.part \
+  --threads 8 \
+  --seed 38745 \
+  --bs-trees 100 \
+  --prefix tree/04_bootstrap
