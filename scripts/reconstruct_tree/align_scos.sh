9109173bc0df45b74022fbbbedb42418adeb0e16
HobnobMancer <eemh1@st-andrews.ac.uk>
2021-11-11 11:13:20 +0000

    add path as arg not hard coded

--- scripts/reconstruct_tree/align_scos.sh
+++ scripts/reconstruct_tree/align_scos.sh
@@ -4,11 +4,13 @@
 #
 # Align single-copy orthologue sequences using MAFFT
 
+$1  # directory containing output from orthofinder, e.g. orthologues/Results_Nov11/Single_Copy_Orthologue_Sequences
+
 # Create output directory
 mkdir -p sco_proteins_aligned
 
 # Align each set of SCOs
-for fname in orthologues/Results_May28/Single_Copy_Orthologue_Sequences/*.fa
+for fname in $1/*.fa
 do
     mafft --thread 12 ${fname} > sco_proteins_aligned/`basename ${fname%%.fa}`_aligned.fasta
 done
