a3b0317fc19ddffd37046cf1a861033d652c233c
HobnobMancer <hobnobmancer@Paddington.localdomain>
2022-01-28 16:13:04 +0000

    turn hard coded paths into cmd args

--- scripts/reconstruct_tree/ml_tree/raxml_ng_build_tree.sh
+++ scripts/reconstruct_tree/ml_tree/raxml_ng_build_tree.sh
@@ -47,28 +47,30 @@
 # Build maximum parsimony tree using raxml-ng, with
 # bootstrap support
 
+$1  # dir containing concatenated cds sequences
+
 mkdir tree
 
 raxml-ng --check \
-  --msa concatenated_cds/concatenated.fasta \
-  --model concatenated_cds/concatenated.part \
+  --msa $1/concatenated.fasta \
+  --model $1/concatenated.part \
   --prefix tree/01_check
 
 raxml-ng --parse \
-  --msa concatenated_cds/concatenated.fasta \
-  --model concatenated_cds/concatenated.part \
+  --msa $1/concatenated.fasta \
+  --model $1/concatenated.part \
   --prefix tree/02_parse
 
 raxml-ng \
-  --msa concatenated_cds/concatenated.fasta \
-  --model concatenated_cds/concatenated.part \
+  --msa $1/concatenated.fasta \
+  --model $1/concatenated.part \
   --threads 8 \
   --seed 38745 \
   --prefix tree/03_infer
 
 raxml-ng --bootstrap \
-  --msa concatenated_cds/concatenated.fasta \
-  --model concatenated_cds/concatenated.part \
+  --msa $1/concatenated.fasta \
+  --model $1/concatenated.part \
   --threads 8 \
   --seed 38745 \
   --bs-trees 100 \
